77cea276223a9fdc70f43048ce393afa5d95c059
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-09-22 15:27:32 +0800

    chore: update test

--- simple-test.ts
+++ simple-test.ts
@@ -4,8 +4,24 @@ const app = new Verre()
 
 app.get('/', () => ({
   status: 200,
+  headers: {
+    'Content-Type': 'text/plain',
+  },
   body: 'Hello from Verre!'
 }))
+
+app.get('/id/{id}', (_err, req) => {
+  const id = new URL(req.url).pathname.slice(4)
+  const name = new URLSearchParams(req.query ?? '').get('name')
+
+  return {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    body: `${id} ${name}`
+  }
+})
 
 app.get('/json', async (_err, req) => {
   const json: unknown = await req.json()

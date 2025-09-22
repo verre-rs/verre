8bd1ed4be25f6841216f1de5a795c469437c3776
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-09-22 14:53:00 +0800

    feat(request): json, query, headers method

--- src/request.rs
+++ src/request.rs
@@ -1,9 +1,12 @@
+use std::collections::HashMap;
+
 use axum::{
   body::{to_bytes, Body},
   http::Request,
 };
 use napi::bindgen_prelude::Uint8Array;
 use napi_derive::napi;
+use serde_json::Value;
 
 #[napi(js_name = "Request")]
 pub struct VerreRequest(Request<Body>);
@@ -28,8 +31,42 @@ impl VerreRequest {
     }
   }
 
+  #[napi]
+  pub async unsafe fn json(&mut self) -> Option<Value> {
+    let body = std::mem::take(self.0.body_mut());
+
+    let bytes = match to_bytes(body, usize::MAX).await {
+      Ok(bytes) => bytes,
+      Err(_) => return None,
+    };
+
+    match serde_json::from_slice(&bytes) {
+      Ok(json_value) => Some(json_value),
+      Err(_) => None,
+    }
+  }
+
   #[napi(getter)]
   pub fn url(&mut self) -> String {
     self.0.uri().to_string()
+  }
+
+  #[napi(getter)]
+  pub fn query(&mut self) -> Option<String> {
+    self.0.uri().query().map(|query| query.to_string())
+  }
+
+  #[napi(getter)]
+  pub fn headers(&mut self) -> HashMap<String, String> {
+    self
+      .0
+      .headers()
+      .into_iter()
+      .filter_map(|(key, value)| {
+        let key = key.to_string();
+        let value = value.to_str().unwrap().to_string();
+        Some((key, value))
+      })
+      .collect()
   }
 }
